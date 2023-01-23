cd85b3dc2825aeaabdbe6be69289df862959d233
Vitaliy Roschin <kot.official@yandex.ru>
2023-01-24 01:50:56 +0300

    исправил подключение rateLimit

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -1,6 +1,6 @@
-const { rateLimit } = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 
-module.exports = rateLimit({
+module.exports.rateLimit = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
