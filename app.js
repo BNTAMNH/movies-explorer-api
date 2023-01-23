89683f14e47bd0da7a263b548f62e4ce3b9a5796
Vitaliy Roschin <kot.official@yandex.ru>
2023-01-23 23:13:48 +0300

    добавил конфиг для cors и ограничение запросов

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('./middlewares/cors');
 const router = require('./routes/index');
+const { devMongoUrl } = require('./utils/config');
+const { rateLimit } = require('./utils/rateLimit');
+
+const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : devMongoUrl);
 
 const app = express();
-app.use(bodyParser.json());
-
-const { PORT = 3000 } = process.env;
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.use(requestLogger);
+app.use(rateLimit);
+app.use(helmet());
+app.use(cors);
+app.use(bodyParser.json());
+
 app.use('/', router);
 
 app.use(errorLogger);
--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
+const { jwtDev } = require('../utils/config');
 
 const User = require('../models/user');
 
@@ -40,7 +41,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : jwtDev, { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => {
